57612b88169152cd8ca1ab0329546aefb355be93
Chad Shryock <chad@keystone.works>
2017-07-28 11:12:24 -0400

    Fix for Django 1.10+

--- googleauth/urls.py
+++ googleauth/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import *
+from django.conf.urls import url
 
-urlpatterns = patterns('',
+
+urlpatterns = [
     url(r'^login/$', 'googleauth.views.login', name='googleauth_login'),
     url(r'^callback/$', 'googleauth.views.callback', name='googleauth_callback'),
     url(r'^logout/$', 'googleauth.views.logout', name='googleauth_logout'),
-)
+]
